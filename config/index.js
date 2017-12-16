1b36f160b8415463935b910f8b6f23081b45096c
DonutDeflector <donutdeflector@tuta.io>
2017-12-15 21:25:45 -0500

    test deployment

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/mendeleev/dist',
     proxyTable: {},
 
     // Various Dev Server settings
